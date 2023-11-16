4c324b62e1aee200d142f55d690a86ed0344022a
moon.dev <moon.dev@kakaomibility.com>
2023-11-16 17:00:21 +0900

    init backend

---
+++ backend-spring/src/main/java/com/frankie/kamomatzip/KamomatzipApplication.java
@@ -0,0 +1,13 @@
+package com.frankie.kamomatzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamomatzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamomatzipApplication.class, args);
+	}
+
+}
